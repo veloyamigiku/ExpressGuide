bfc836e6d8b254249a08d8bb6b453a6fb1dac880
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2025-04-21 15:33:24 +0900

    ・Expressのガイド【Error Handling】のスクリプト【Catching Errors】を修正する。

--- ErrorHandling/catch_errors.js
+++ ErrorHandling/catch_errors.js
@@ -33,6 +33,49 @@ app.get('/user/:id', async (req, res, next) => {
   res.send(user);
 });
 
+app.get('/write', [
+  function (req, res, next) {
+    //fs.writeFile('./inaccessible-path', 'data', next);
+    fs.writeFile('./test/inaccessible-path', 'data', next);
+  },
+  function (req, res) {
+    res.send('OK');
+  }
+]);
+
+app.get('/next_err', (req, res, next) => {
+  setTimeout(() => {
+    try {
+      throw new Error('BROKEN');
+    } catch (err) {
+      next(err);
+    }
+  }, 1000);
+});
+
+app.get('/promise_err', (req, res, next) => {
+  new Promise((resolve, reject) => {
+    //throw new Error('Promise_main_err.');
+    resolve();
+  }).then(() => {
+    throw new Error('Promise_resolve_then_err.');
+  }).catch(next);
+});
+
+app.get('/read', [
+  function (req, res, next) {
+    fs.readFile('/maybe-valid-file', 'utf-8', (err, data) => {
+    //fs.readFile('./catch_errors.js', 'utf-8', (err, data) => {
+      res.locals.data = data;
+      next(err);
+    });
+  },
+  function (req, res) {
+    res.locals.data = res.locals.data.split(',')[1];
+    res.send(res.locals.data);
+  }
+]);
+
 app.listen(
   port,
   () => {
